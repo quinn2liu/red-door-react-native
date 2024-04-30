d1e1b3f4428b1469aa64e477365da71cb7ec845f
Quinn Liu <quinnliu@ist-6918.apng.wlan.private.upenn.edu>
2024-04-30 12:42:14 -0400

    first commit

---
+++ components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+
+import { Images } from '../config';
+
+export const Logo = ({ uri }) => {
+  return <Image source={uri} style={styles.image} />;
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: 150,
+    height: 180
+  }
+});
